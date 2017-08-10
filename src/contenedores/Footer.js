7b4f8ac8c21bbd5f4ebc4ad0442ec30060f10a14
AngelicaMVG <angie.velazquez2211@gmail.com>
2017-08-10 09:51:19 -0500

    color de botones

--- src/contenedores/Footer.js
+++ src/contenedores/Footer.js
@@ -12,17 +12,17 @@ export default class Footer extends Component {
             <a href="http://www.softtek.com/" className=' btn-floating  waves-effect btn-large btn-logo white'>
               <span><img className="logo" src={logo} alt='logo'/></span>
             </a>
-            <a href="https://www.linkedin.com/company/softtek" className='tooltipped btn-floating  waves-effect btn-large btn'>
-              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            <a href="https://www.linkedin.com/company/softtek" className=' btn-floating  waves-effect btn-large btn'>
+              <i className="fa fa-linkedin"></i>
             </a>
-            <a href="https://twitter.com/Softtek" className='tooltipped btn-floating  waves-effect btn-large btn'>
-              <i className="fa fa-twitter " aria-hidden="true"></i>
+            <a href="https://twitter.com/Softtek" className=' btn-floating  waves-effect btn-large btn'>
+              <i className="fa fa-twitter " ></i>
             </a>
-            <a href="https://www.facebook.com/softtek" className='tooltipped btn-floating  waves-effect btn-large btn'>
+            <a href="https://www.facebook.com/softtek" className=' btn-floating  waves-effect btn-large btn'>
               <i className="fa fa-facebook "></i>
             </a>
-            <a href="https://www.youtube.com/user/SofttekTV" className='tooltipped btn-floating  waves-effect btn-large btn'>
-              <i className="fa fa-youtube-play " aria-hidden="true"></i>
+            <a href="https://www.youtube.com/user/SofttekTV" className=' btn-floating  waves-effect btn-large btn'>
+              <i className="fa fa-youtube-play "></i>
             </a>
           </Col>
         </Row>
