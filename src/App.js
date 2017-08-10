ae66838ad420ce4b79e029b5a2a1bb23b6aa92a9
noemi <noemiarismendi.cc@gmail.com>
2017-08-09 19:57:32 -0500

    formulario para crear publicacion listo

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {Link, Route} from 'react-router-dom';
 import Home from './contenedores/Home.jsx';
 import About from './contenedores/About.jsx';
 import Vista from './contenedores/Vista.jsx';
+import Formulario from './contenedores/Formulario.jsx';
 import Nav from './contenedores/Navegador.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className=''>
-          <Nav/>
+          
         <div>
           <div>
             <ul>
@@ -25,6 +27,9 @@ class App extends Component {
               <li>
                 <Link to="/vista">Topics</Link>
               </li>
+              <li>
+                <Link to="/formulario">Formulario</Link>
+              </li>
             </ul>
 
             <hr/>
@@ -32,6 +37,7 @@ class App extends Component {
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/vista" component={Vista}/>
+            <Route path="/formulario" component={Formulario}/>
           </div>
         </div>
       </div>
