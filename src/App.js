e5906c2ffc6d431fdcae82ea94c6a0ddb0712bed
noemi <noemiarismendi.cc@gmail.com>
2017-08-10 16:59:46 -0500

    modificando filtro tag

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
                 <Link to="/about">About</Link>
                 </li>
                 <li>
-                <Link to="/vista">Topics</Link>
+                <Link to="/vista">FiltroTags</Link>
                 </li>
                 <li>
                  <Link to="/detalle">Detalle</Link>
@@ -40,7 +40,7 @@ class App extends Component {
             </ul>
 
             <hr/>
-        <Footer/>
+        
 
 
 
@@ -48,6 +48,7 @@ class App extends Component {
             <Route path="/vista" component={Vista}/>
             <Route path="/tarjetaPublicacion" component={TarjetaPublicacion}/>
             <Route path="/detalle" component={Detalle}/>
+            <Footer/>
         </div>
     );
   }
