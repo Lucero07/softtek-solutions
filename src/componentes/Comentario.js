3ec4d2bc3738329f606cb35c750c50e2afd81a92
Lucero07 <ing.lucero.ramos@gmail.com>
2017-08-10 17:18:19 -0500

    vinculando vistas

--- src/componentes/Comentario.js
+++ src/componentes/Comentario.js
@@ -6,7 +6,7 @@ export default class Comentario extends Component{
 		const name =this.props.name;
 		return (
 			<Col m={6} s={12}>
-				<Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>{name}</a>]}>
+				<Card className='blue-grey darken-1' textClassName='white-text' title='Card title' actions={[<a href='#'>Autor</a>]}>
 				{name}
 				</Card>
 		</Col>
