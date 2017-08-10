76cd263c63ef8249b299b894c9936ec5602984b5
Mone Rivas <mone.rivas@gmail.com>
2017-08-09 20:11:57 -0500

    Pantalla de Login con imagen sin optimizar

--- src/App.js
+++ src/App.js
@@ -7,34 +7,15 @@ import Home from './contenedores/Home.jsx';
 import About from './contenedores/About.jsx';
 import Vista from './contenedores/Vista.jsx';
 import Nav from './contenedores/Navegador.js';
+import Login from './contenedores/Login.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className=''>
-          <Nav/>
-        <div>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/vista">Topics</Link>
-              </li>
-            </ul>
-
-            <hr/>
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/vista" component={Vista}/>
-          </div>
+        <div className="App">
+            <Login/>
+            
         </div>
-      </div>
     );
   }
 }
